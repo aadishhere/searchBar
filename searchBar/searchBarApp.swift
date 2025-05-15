dc74f0b265a329093bfb1f6494eef6da731e094a
Aadish Jain <aadishhere@gmail.com>
2025-05-15 11:47:30 +0530

    Initial Commit

---
+++ searchBar/searchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  searchBarApp.swift
+//  searchBar
+//
+//  Created by Aadish Jain on 15/05/25.
+//
+
+import SwiftUI
+
+@main
+struct searchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
